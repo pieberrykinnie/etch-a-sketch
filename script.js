4441f78671ecf8ebcea720e431132566e9d09bdf
Peter Vu <dmtruong1980@gmail.com>
2024-03-05 23:22:03 -0600

    feat: Add button to customize grid's size

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 const container = document.querySelector("#container");
 
-const ROW_COUNT = 16;
-const COL_COUNT = 16;
+let rowCount = 16;
+let colCount = 16;
 
-for (let i = 0; i < ROW_COUNT; i++) {
+function generateGrid(row, col) {
+  for (let i = 0; i < row; i++) {
     const rowCoordinates = "row-" + i;
     const row = document.createElement("div");
-    
+
     row.className = rowCoordinates;
     container.appendChild(row);
 
-    for (let j = 0; j < COL_COUNT; j++) {
-        const cellCoordinates = "cell-" + i + "-" + j;
-        const cell = document.createElement("div");
-        
-        cell.className = cellCoordinates;
-        row.appendChild(cell);
+    for (let j = 0; j < col; j++) {
+      const cellCoordinates = "cell-" + i + "-" + j;
+      const cell = document.createElement("div");
+
+      cell.className = cellCoordinates;
+      row.appendChild(cell);
     }
-}
+  }
 
-const cellList = container.querySelectorAll("#container > div > div");
+  const cellList = container.querySelectorAll("#container > div > div");
 
-for (const cell of cellList) {
+  for (const cell of cellList) {
     cell.addEventListener("mouseenter", (e) => {
-        e.target.classList.add("focus");
-    })
+      e.target.classList.add("focus");
+    });
 
     cell.addEventListener("mouseleave", (e) => {
-        e.target.classList.remove("focus");
-    })
+      e.target.classList.remove("focus");
+    });
+  }
 }
+
+generateGrid(rowCount, colCount);
+
+const resetButton = document.querySelector("#reset-grid");
+
+resetButton.addEventListener("click", () => {
+    let userInput;
+
+    do {
+        userInput = Number(prompt("Please enter the dimension of the grid as a number less than 100:"));
+    } while (isNaN(userInput) || userInput <= 0 || userInput > 100);
+
+    container.replaceChildren();
+
+    generateGrid(userInput, userInput);
+});
